788557f9a38013ffc50af7491236d4d78bda692f
MDobrilko <51864113+MDobrilko@users.noreply.github.com>
2021-09-22 21:22:32 +0300

    added solution for simple task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use algorithms;
+
+fn task(n: usize) {
+    let mut heap = algorithms::Heap::new();
+
+    [2, 3, 5].iter().for_each(|elem| heap.push(*elem));
+
+    for _ in 0..n {
+        let v = *heap.top().unwrap();
+        while *heap.top().unwrap() == v { heap.pop(); }
+
+        print!("{} ", v);
+        for i in [v * 2, v * 3, v * 5] {
+            heap.push(i);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello world");
+    task(10);
 }
